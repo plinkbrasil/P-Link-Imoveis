e7ef9863c6b7c693a666280c79779ea26da5e534
P-Link Imóveis <p.linkbrasil@gmail.com>
2026-01-21 18:31:37 -0300

    Update page.tsx

--- app/evento/page.tsx
+++ app/evento/page.tsx
@@ -11,15 +11,16 @@ export const metadata = {
 export default function EventoPage() {
   return (
     <main className="min-h-screen flex items-top pt-32 justify-center bg-gradient-to-br from-[#0c0f14] to-[#0a454f] px-4">
-      <div className="max-w-xl w-full text-center space-y-6 text-white">
 
-        {/* Brasão */}
+    {/* Brasão */}
     <img
       src="/brasao.png"
       alt="Brasão Dyba"
       className="w-full h-auto rounded-xl"
       draggable={false}
     />
+
+      <div className="max-w-xl w-full text-center space-y-6 text-white">
 
         {/* FOTO */}
         <div className="w-full overflow-hidden rounded-2xl">
