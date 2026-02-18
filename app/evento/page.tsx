27c97a16f7be6a29b756b55d570027831aa26a2b
P-Link Imóveis <p.linkbrasil@gmail.com>
2026-02-18 19:08:33 -0300

    Update page.tsx

--- app/evento/page.tsx
+++ app/evento/page.tsx
@@ -44,6 +44,7 @@ export default function EventoPage() {
         O aniversário do Thiago Viola será no sábado, dia 28 de fevereiro, na Chácara Bobato (Umbará, Curitiba), com almoço e música garantidos pelo anfitrião, ficando apenas a bebida por conta de cada convidado.
         </p>
 
+        {/* BOTÃO */}
         <a
   href="https://www.google.com/maps/place/Cerâmica+Iguaçu+Ltda/@-25.6035699,-49.277776,382m/data=!3m1!1e3!4m6!3m5!1s0x94dcf9002d0ad2bd:0x4c16d86ab1f9b27c!8m2!3d-25.6029485!4d-49.2770098!16s%2Fg%2F11w_yg6q35?entry=ttu&g_ep=EgoyMDI2MDIxNi4wIKXMDSoASAFQAw%3D%3D"
   target="_blank"
