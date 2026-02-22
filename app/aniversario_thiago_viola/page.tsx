3980073df261e6e1c82c4e2d59b6aeebeffcb708
P-Link Imóveis <p.linkbrasil@gmail.com>
2026-02-22 17:47:00 -0300

    Update page.tsx

--- app/aniversario_thiago_viola/page.tsx
+++ app/aniversario_thiago_viola/page.tsx
@@ -14,10 +14,12 @@ export const metadata: Metadata = {
         url: "https://www.p-linkimoveis.com.br/thiagoog.jpg",
         width: 1200,
         height: 630,
+        type: "image/jpg",
       },
     ],
     type: "website",
-  },
+    
+  }, 
   twitter: {
     card: "summary_large_image",
     title: "Aniversário do Thiago Viola",
